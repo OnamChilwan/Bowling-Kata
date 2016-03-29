7be1d24f2d7a1e8106fe510d2960f68f66d021c3
Onam Chilwan <onamch@gmail.com>
2016-03-29 16:59:20 +0100

    started styling the console app, bit difficult not my forte

--- src/BowlingGame/Program.cs
+++ src/BowlingGame/Program.cs
@@ -1,9 +1,58 @@
 ﻿namespace BowlingGame
 {
+    using System;
+
+    using BowlingGame.Models;
+
     class Program
     {
+        private static Game game;
+
         static void Main(string[] args)
         {
+            game = new Game();
+
+            for (int i = 1; i < 10; i++)
+            {
+                var pinsKnocked = int.Parse(Write("Enter number of pins knocked: "));
+                game.Roll(pinsKnocked);
+                Console.Clear();
+
+                Display();
+            }
+        }
+
+        static void Display()
+        {
+            foreach (var frame in game.Frames)
+            {
+                Console.Write("--");
+                Console.Write("\t");
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var frame in game.Frames)
+            {
+                Console.Write(frame.TotalPoints.ToString("00"));
+                Console.Write("\t");
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var frame in game.Frames)
+            {
+                Console.Write("--");
+                Console.Write("\t");
+            }
+        }
+
+        static string Write(string message)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(message);
+
+            return Console.ReadLine();
         }
     }
 }
